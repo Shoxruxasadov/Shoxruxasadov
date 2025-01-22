e3bebf8e03fd39848e243f9761315a98074021ee
Shoxrux Asadov <129892167+Shoxruxasadov@users.noreply.github.com>
2025-01-22 11:40:09 +0500

    test

--- src/components/Main/Education.js
+++ src/components/Main/Education.js
@@ -8,39 +8,43 @@ export default function Education() {
     <section className="section section-three">
       <div className="education">
         <p>{t("section-three.edu")}</p>
-        <div className="edu tuit">
-          <img src="/images/education/tatu.png" />
-          <div className="edu-title">
-            <h1>{t("section-three.tuitName")}</h1>
-            <h2>{t("section-three.tuitDire")}</h2>
-            <h3>{t("section-three.tuitYear")}</h3>
+        <div className="wrapper">
+          <div className="edu tuit">
+            <img src="/images/education/tatu.png" />
+            <div className="edu-title">
+              <h1>{t("section-three.tuitName")}</h1>
+              <h2>{t("section-three.tuitDire")}</h2>
+              <h3>{t("section-three.tuitYear")}</h3>
+            </div>
           </div>
-        </div>
-        <div className="edu shift">
-          <img src="/images/education/shift.png" />
-          <div className="edu-title">
-            <h1>{t("section-three.shiftName")}</h1>
-            <h2>{t("section-three.shiftDire")}</h2>
-            <h3>{t("section-three.shiftYear")}</h3>
+          <div className="edu shift">
+            <img src="/images/education/shift.png" />
+            <div className="edu-title">
+              <h1>{t("section-three.shiftName")}</h1>
+              <h2>{t("section-three.shiftDire")}</h2>
+              <h3>{t("section-three.shiftYear")}</h3>
+            </div>
           </div>
         </div>
       </div>
       <div className="experience">
         <p>{t("section-three.exp")}</p>
-        <div className="exp regular">
-          <img src="/images/education/regular.png" />
-          <div className="exp-title">
-            <h1>{t("section-three.regularName")}</h1>
-            <h2>{t("section-three.regularDire")}</h2>
-            <h3>{t("section-three.regularYear")}</h3>
+        <div className="wrapper">
+          <div className="exp regular">
+            <img src="/images/education/regular.png" />
+            <div className="exp-title">
+              <h1>{t("section-three.regularName")}</h1>
+              <h2>{t("section-three.regularDire")}</h2>
+              <h3>{t("section-three.regularYear")}</h3>
+            </div>
           </div>
-        </div>
-        <div className="exp kwork">
-          <img src="/images/education/empire.png" />
-          <div className="exp-title">
-            <h1>{t("section-three.empireName")}</h1>
-            <h2>{t("section-three.empireDire")}</h2>
-            <h3>{t("section-three.empireYear")}</h3>
+          <div className="exp kwork">
+            <img src="/images/education/empire.png" />
+            <div className="exp-title">
+              <h1>{t("section-three.empireName")}</h1>
+              <h2>{t("section-three.empireDire")}</h2>
+              <h3>{t("section-three.empireYear")}</h3>
+            </div>
           </div>
         </div>
       </div>
